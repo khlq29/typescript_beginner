71d4d944694714bfde879ba6a131eaf52451b37a
khlq29 <asimasu31@gmail.com>
2024-03-16 21:48:58 +0530

    update

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,27 @@
-// // var _ = require('lodash')
-// import _ from 'lodash'
-// console.log(_.shuffle([1, 2, 3]));
-
-// in app.ts
-// using plainToInstance instead of plainToClass
-// with class-transformer v0.5.1+
-
-import 'reflect-metadata';
-import { plainToInstance } from 'class-transformer';
-import {validate} from 'class-validator'
-
-import { Product } from './product.model'
-
-
-const newProd = new Product('', -9999)
-validate(newProd).then(errors => {
-    if (errors.length > 0) {
-        console.log('VALIDATION ERROR!');
-        console.log(errors);
-    } else {
-        console.log(newProd.getInformation());
-   }
-    
-})
-
-
-const products = [
-    { title: 'Chizuru', price: 29.99 },
-    { title: 'Yukinon', price:99.99}
-];
-// const p1 = new Product('Oshi no ko', 12.99)
-
-// const loaderProducts = products.map(prod => {
-//     return new Product(prod.title,prod.price)
-// })
-
-const loaderProducts = plainToInstance(Product, products)
-
-for (const prod of loaderProducts) {
-    console.log(prod.getInformation());
+// import axios from "axios";
+// import ol from 'ol'
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+declare var ol: any;
+ 
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+ 
+  const coordinates = {lat: 40.41, lng: -73.99}; // Can't fetch coordinates from Google API, use dummy ones
+ 
+  document.getElementById('map')!.innerHTML = ''; // clear <p> from <div id="map">
+  new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 16
+    })
+  });
 }
+
+form.addEventListener("submit", searchAddressHandler);
